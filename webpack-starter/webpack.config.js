b3ab2be6860fe030538b8d1c3112cad04b5324fb
cakescripter <cakescripter@gmail.com>
2023-07-30 01:56:06 +0200

    Commit

--- webpack-starter/webpack.config.js
+++ webpack-starter/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
@@ -21,7 +23,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.js$/,
@@ -40,6 +42,7 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new MiniCssExtractPlugin()
     ]
 }
